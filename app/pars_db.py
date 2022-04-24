4f801817e8f49ecf7675b95a21029cd6240c9c46
e-razdumina <e.razdumina@gmail.com>
2022-04-24 20:27:48 +0300

    changed pars_db logic

--- app/pars_db.py
+++ app/pars_db.py
@@ -6,13 +6,7 @@ client = MongoClient('localhost', 27017)
 db = client.flask_db
 todos = db.valhalla
 
-vikings_tv = vikings_tv_pars()
-nors_tv = norsemen_tv_pars_wiki()
-nors_team = norsemen_nfl_wiki()
-
-info = [vikings_tv, nors_tv, nors_team]
-
-for i in info:
-    todos.insert_many(i)
-
+vikings_tv = todos.insert_many(vikings_tv_pars())
+nors_tv = todos.insert_many(norsemen_tv_pars_wiki())
+nors_team = todos.insert_many(norsemen_nfl_wiki())
 
