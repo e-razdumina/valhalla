dedb9f1f3f66599030a7666a5b0f6a7da96c8c4d
e-razdumina <e.razdumina@gmail.com>
2022-04-24 20:42:25 +0300

    changed Mongo set up

--- app/pars_db.py
+++ app/pars_db.py
@@ -1,7 +1,7 @@
 from pars_functions import vikings_tv_pars, norsemen_tv_pars_wiki, norsemen_nfl_wiki
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('localhost', 27017, tls=True)
 
 db = client.flask_db
 todos = db.valhalla
